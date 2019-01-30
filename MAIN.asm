b9e4e170db74e970fdae041016d0bee1411adc5e
RazBrutman <razbrutman@gmail.com>
2019-01-30 12:49:28 +0200

    Cleaning DATASEG

--- MAIN.asm
+++ MAIN.asm
@@ -7,107 +7,109 @@ STACK 100h
 DATASEG
 ; --------------------------
 ; Your variables here
-; <name>	<size>		<value>
-;			db/dw/dd	?/*
-	save						db		18 dup (0)
+;	<name>		<size>		<value>
+;				db/dw/dd	?/*
+	sav			db			18 dup (0)
 	
-	counter				dw		0
+	counter		dw			0
 	
-	score					dw		0
-	lines						dw		0
-	level						db		1
+	score		dw			0
+	lines		dw			0
+	level		db			1
 
-	delayT					dw		100d
+	delayT		dw			100d
 
-	flipedForm			dw		?
+	flipedForm	dw			?
 
-	canFlip				dw		0h
+	canFlip		dw			0h
 
-	mapColor				db		?
-;													1							2							3							4							5							6							7
-;												[Blue]					[Green]				[Cyan]				[Red]					[Purple]				[Orange]			[Yellow]
-	RGB					db		00h, 80h, 0ffh,		00h, 0ffh, 00h,		00h, 0ffh, 0ffh,		0ffh, 00h, 00h,		33h, 00h, 0ffh,		0ffh, 90h, 00h,		0ffh, 0ffh, 00h
-								db		00h, 00h, 099h,	00h, 099h, 00h,	00h, 0aah, 0aah,	099h, 00h, 00h,	11h, 00h, 0cch,	099h, 4ch, 00h,	099h, 099h, 00h
-;												[White]
-								db		0ffh, 0ffh, 0ffh
-	loopcount1			db		?
-	loopcount2			db		?
+	mapColor	db			?
+;								1						2					3					4						5						6					7
+;								[Blue]					[Green]				[Cyan]				[Red]					[Purple]				[Orange]			[Yellow]
+	RGB			db			00h, 80h, 0ffh,			00h, 0ffh, 00h,		00h, 0ffh, 0ffh,	0ffh, 00h, 00h,			33h, 00h, 0ffh,			0ffh, 90h, 00h,		0ffh, 0ffh, 00h
+				db			00h, 00h, 099h,			00h, 099h, 00h,		00h, 0aah, 0aah,	099h, 00h, 00h,			11h, 00h, 0cch,			099h, 4ch, 00h,		099h, 099h, 00h
+;								[White]
+				db			0ffh, 0ffh, 0ffh
 	
-	PauseStr				db		'PAUSE$'
-	nextStr				db		'NEXT PIECE$'
-	loseStr					db		'YOU LOST!$'
-	T10Str				db		'TOP 10 SCORES:$'
-	ScoreStr				db		'SCORE$'
-	LinesStr				db		'LINES$'
-	LevelStr				db		'LEVEL$'
-	HighScoreStr		db		'NEW HIGHSCORE! (PRESS ENTER TO CONTINUE)$'
-	EnterNameStr		db		'ENTER NAME (THREE DIGITS): $'
-	EnterLevelStr		db		'ENTER START LEVEL (01 - 15): $'
-	nme 					db 		6 dup (?)
+	loopcount1	db			?
+	loopcount2	db			?
+	
+	PauseStr	db			'PAUSE$'
+	nextStr		db			'NEXT PIECE$'
+	loseStr		db			'YOU LOST!$'
+	T10Str		db			'TOP 10 SCORES:$'
+	ScoreStr	db			'SCORE$'
+	LinesStr	db			'LINES$'
+	LevelStr	db			'LEVEL$'
+	HighScoreStr	db		'NEW HIGHSCORE! (PRESS ENTER TO CONTINUE)$'
+	EnterNameStr	db		'ENTER NAME (THREE DIGITS): $'
+	EnterLevelStr	db		'ENTER START LEVEL (01 - 15): $'
+	
+	nme 		db 			6 dup (?)
 	
 	
-	Clock					equ		es:6Ch
+	Clock equ es:6Ch
 
-	x 							dw 		0
-	y							dw		0
-	color 					db		4
-	nextColor			db		1
-	sizeX					dw		?
-	temp					dw		?
-	tempForm			dw		?
+	x			dw 			0
+	y			dw			0
+	color 		db			4
+	nextColor	db			1
+	sizeX		dw			?
+	temp		dw			?
+	tempForm	dw			?
 
 	
-	sizeY					dw		?
-	count					dw		0h
-	adress  				dw		?
-	check					dw		0h
-	timeToCheck		db		0
+	sizeY		dw			?
+	count		dw			0h
+	adress  	dw			?
+	check		dw			0h
+	timeToCheck	db			0
 	
-	form						dw		?	
-	nextForm			dw		0
+	form		dw			?	
+	nextForm	dw			0
 	
-;														J									S									I									Z									T									L									O
-	formwz				dw		0000000010001110b, 0000000001101100b, 0000000000001111b, 0000000011000110b, 0000000001001110b, 0000000000101110b, 0000000011001100b
-	
-	
+;									J					S					I					Z				T					L					O
+	formwz		dw			0000000010001110b, 0000000001101100b, 0000000000001111b, 0000000011000110b, 0000000001001110b, 0000000000101110b, 0000000011001100b
 	
 	
-	checkType			db		1
 	
-	filename				db		'top10.txt',0
-	filehandle				dw		?
+	
+	checkType	db			1
+	
+	filename	db			'top10.txt',0
+	filehandle	dw			?
 
-	ErrorMsg			db		'Error', 10, 13,'$'
+	ErrorMsg	db			'Error', 10, 13,'$'
 	
 	
-	Buffer					db		'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0
+	Buffer		db			'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0, 'AAA', 0, 0
 	
-	print_dec				db		0,0,0,0,0,'$'
-	score_arr				db		0,0,0,0,0
+	print_dec	db			0,0,0,0,0,'$'
+	score_arr	db			0,0,0,0,0
 	
-	note						dw		9121 ; 1193180 / 131 -> (hex)
+	note		dw			9121 ; 1193180 / 131 -> (hex)
 	
 ;-------------------------------;
 	MAX_BMP_WIDTH equ 320
 	MAX_BMP_HEIGHT equ 200
 ;-------------------------------;
-	OneBmpLine db MAX_BMP_WIDTH dup (0)  ; One Color line read buffer
-	ScreenLineMax db MAX_BMP_WIDTH dup (0)  ; One Color line read buffer
-	Header db 54 dup(0)
-	Pallette db 400h dup (0)
+	OneBmpLine		db		MAX_BMP_WIDTH dup (0)  ; One Color line read buffer
+	ScreenLineMax	db		MAX_BMP_WIDTH dup (0)  ; One Color line read buffer
+	Header 			db		54 dup(0)
+	Pallette 		db		400h dup (0)
 ;-------------------------------;
-	StartScreenName db 'starter.bmp', 0
-	InstructionsName1 db 'inst.bmp', 0
-	InstructionsName2 db 'inst2.bmp', 0
+	StartScreenName		db	'starter.bmp', 0
+	InstructionsName1	db	'inst.bmp', 0
+	InstructionsName2 	db	'inst2.bmp', 0
 ;-------------------------------;
-	BmpFileErrorMsg db 'Error At Opening Bmp File .', 0dh, 0ah,'$'
-	ErrorFile db 0
-	BB db "BB..",'$'
+	BmpFileErrorMsg	db		'Error At Opening Bmp File .', 0dh, 0ah,'$'
+	ErrorFile		db		0
+	BB 				db		"BB..",'$'
 ;-------------------------------;
-	BmpLeft dw ?
-	BmpTop dw ?
-	BmpColSize dw ?
-	BmpRowSize dw ?
+	BmpLeft		dw			?
+	BmpTop		dw			?
+	BmpColSize	dw			?
+	BmpRowSize	dw			?
 ; --------------------------
 
 CODESEG
